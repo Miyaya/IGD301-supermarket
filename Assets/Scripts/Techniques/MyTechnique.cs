659143629243f7b14319769901d21ba272fd1490
Miya <miya850604@gmail.com>
2024-01-26 15:57:10 +0100

    able to select by pinching index finger

--- Assets/Scripts/Techniques/MyTechnique.cs
+++ Assets/Scripts/Techniques/MyTechnique.cs
@@ -5,17 +5,45 @@ using UnityEngine;
 // Your implemented technique inherits the InteractionTechnique class
 public class MyTechnique : InteractionTechnique
 {
-    // You must implement your technique in this file
-    // You need to assign the selected Object to the currentSelectedObject variable
-    // Then it will be sent through a UnityEvent to another class for handling
+    [SerializeField]
+    int raycastMaxDistance = 1000;
+
+    [SerializeField]
+    private OVRHand rightHand;
+
+    private LineRenderer lineRenderer;
+    private bool isIndexFingerPinching;
     private void Start()
     {
-        // TODO
+        lineRenderer = rightHand.GetComponent<LineRenderer>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        //TODO : Select a GameObject and assign it to the currentSelectedObject variable
+        Transform rightHandTransform = rightHand.transform;
+        var pos = rightHandTransform.position;
+        lineRenderer.SetPosition(0, pos);
+
+        var left = -rightHandTransform.right; // NOTE: Idk why .forward doesn't really work, instead have to use left
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(pos, left, out hit, Mathf.Infinity);
+        isIndexFingerPinching = rightHand.GetFingerIsPinching(OVRHand.HandFinger.Index);
+        // Checking that the user pushed the trigger
+        if (isIndexFingerPinching && hasHit)
+        {
+            // Sending the selected object hit by the raycast
+            currentSelectedObject = hit.collider.gameObject;
+        }
+
+        // Determining the end of the LineRenderer depending on whether we hit an object or not
+        if (hasHit)
+        {
+            lineRenderer.SetPosition(1, hit.point);
+        }
+        else
+        {
+            lineRenderer.SetPosition(1, raycastMaxDistance * left);
+        }
 
 
         // DO NOT REMOVE
